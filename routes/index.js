b30a2c51eddbe3596ea4ac0f8197fd6e69d0cebe
martin-creator <martinlubowa@outlook.com>
2022-08-16 07:21:33 +0300

    Created the about page using bootstrap and added the respective routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Collaborative Code Editor - write some come with family!' });
 });
+
+/* GET about page. */
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'Collaborative Code Editor - write some come with family!'})
+})
 
 module.exports = router;
