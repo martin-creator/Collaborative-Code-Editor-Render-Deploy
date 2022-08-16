067e08a0ffd6c999b1be5ad7973be63c19d80546
martin-creator <martinlubowa@outlook.com>
2022-08-16 13:54:04 +0300

    Added form validation using express-validator module

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { body , validationResult} = require('express-validator');
+const bodyParser = require('body-parser');
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,10 +19,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.text());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,81 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var router = express.Router();
+const { check, body, validationResult } = require("express-validator");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Collaborative Code Editor - write some come with family!' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Collaborative Code Editor - write some come with family!",
+  });
 });
 
 /* GET about page. */
-router.get('/about', function(req, res, next){
-  res.render('about', {title: 'Collaborative Code Editor - write some come with family!'})
-})
- 
+router.get("/about", function (req, res, next) {
+  res.render("about", {
+    title: "Collaborative Code Editor - write some come with family!",
+  });
+});
 
 /* GET contact page. */
-router.get('/contact', function(req, res, next){
-  res.render('contact', {title: 'Collaborative Code Editor - write some come with family!'});
-  })
-
+router.get("/contact", function (req, res, next) {
+  res.render("contact", {
+    title: "Collaborative Code Editor - write some come with family!",
+  });
+});
 
 /* POST contact page. */
- router.post('/contact',  function(req, res, next){
-    res.render('thank', {title: 'Collaborative Code Editor - write some come with family!'});
-  })
+router.post("/contact", [body('name').not().isEmpty().withMessage("Empty name"),
+body('email').not().isEmpty().withMessage("Empty email"),
+body('message').not().isEmpty().withMessage("Empty message")], function (req, res, next) {
+
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.body.message);
+
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+     console.log({errors : errors.array()});
+     console.log("Hi Martin We got some errors");
+    res.render("contact", {
+      title: "Collaborative Code Editor - write some come with family!",
+      errorMessages: errors.array()
+    });
+  } else {
+    res.render("thank", { title: "Collaborative Code Editor - write some come with family!" })
+
+  }
+
+
+
+
+
+
+  /*check(req.bodyname).not().isEmpty().withMessage("Empty name");
+  check(req.body.email).not().isEmpty().withMessage("Empty email");
+  check(req.body.message).not().isEmpty().withMessage("Empty message");
+ 
+  var errors = validationResult(req);
+ 
+
+
+  if (errors) {
+    console.log(errors);
+    res.render("contact", {
+      title: "Collaborative Code Editor - write some come with family!",
+      name: req.check.name,
+      email: req.check.email,
+      message: req.check.message,
+      errorMessages: errors,
+    });
+  } else {
+    res.render("thank", {
+      title: "Collaborative Code Editor - write some come with family!",
+    });
+  }*/
+}
+);
 
 module.exports = router;
