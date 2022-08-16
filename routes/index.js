8b511add4ff79f103454e68a8ff16f46774bb358
martin-creator <martinlubowa@outlook.com>
2022-08-16 07:56:22 +0300

    Added views and route handlers for the contact page

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,17 @@ router.get('/', function(req, res, next) {
 router.get('/about', function(req, res, next){
   res.render('about', {title: 'Collaborative Code Editor - write some come with family!'})
 })
+ 
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next){
+  res.render('contact', {title: 'Collaborative Code Editor - write some come with family!'});
+  })
+
+
+/* POST contact page. */
+ router.post('/contact',  function(req, res, next){
+    res.render('thank', {title: 'Collaborative Code Editor - write some come with family!'});
+  })
 
 module.exports = router;
