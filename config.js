cd892279d89f86f592d1f4bb6ebb835596d674ce
martin-creator <martinlubowa@outlook.com>
2023-04-05 17:28:16 +0300

    Added config files

---
+++ config.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+    mailer : {
+        service : 'Gmail',
+        host: "smtp.gmail.com",
+        port: 587,
+        domains: ["gmail.com", "googlemail.com"],
+        auth : {
+            user: 'africaneaglecode@gmail.com',
+            pass : 'africa2022',
+
+        },
+
+    dbConnstring: 'mongodb+srv://martinlubowa:jesus147852@cluster0.s6launr.mongodb.net/codeshare?retryWrites=true&w=majority',
+
+    sessionKey: 'codeAfrica'
+
+    }
+}
+
+// mongodb+srv://martinlubowa:jesus147852@cluster0.s6launr.mongodb.net/shop?retryWrites=true&w=majority"
